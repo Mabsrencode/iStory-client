f6fbd403ab92dc00ac24ba46170a48bbe40009fc
Reniel George Mababa <mabsrencode@gmail.com>
2024-01-17 06:14:26 +0800

    fix url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const url = "http://https://istory-server.onrender.com:4000/posts";
+const url = "https://istory-server.onrender.com/posts";
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
