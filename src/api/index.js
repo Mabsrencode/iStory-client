bb87a1faadda5ee1d676873b3e99f78171156f01
Reniel George Mababa <mabsrencode@gmail.com>
2024-01-19 11:50:44 +0800

    m

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const url =
-  "http://localhost:4000/posts" || "https://istory-server.onrender.com/posts";
+const url = "https://istory-server.onrender.com/posts";
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) =>
