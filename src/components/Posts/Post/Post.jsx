a2bee23be5938c9e4f6218f17b7ad371f3371d60
Reniel George Mababa <mabsrencode@gmail.com>
2024-01-19 21:11:48 +0800

    post card

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -24,7 +24,7 @@ const Post = ({ post, setCurrentId }) => {
     const handleOpen = () => setOpen((cur) => !cur);
     return (
         <div>
-            <Dialog size="xl" open={open} handler={handleOpen} >
+            <Dialog size="xl" open={open} handler={handleOpen}>
                 <DialogHeader className="justify-between">
                     <div className="flex items-center gap-3">
                         <Avatar
@@ -66,15 +66,12 @@ const Post = ({ post, setCurrentId }) => {
                                 <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                             </svg>
                         </IconButton>
-                        <Button color="gray" size="sm">
-                            Free Download
-                        </Button>
                     </div>
                 </DialogHeader>
                 <DialogBody>
                     <img
                         alt="nature"
-                        className="h-[48rem] w-full rounded-lg object-cover object-top"
+                        className="h-[48rem] md:w-[30%] rounded-lg object-cover object-top"
                         src={post.selectedFile}
                     />
                 </DialogBody>
